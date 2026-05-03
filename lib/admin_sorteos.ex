f7f0f839525910e31e4ce097effede4444f32f95
Cristian Navarro <navarro_cristian@outlook.com>
2026-05-02 22:28:36 -0500

    Implementar lógica de administración de sorteos y persistencia JSON

--- lib/admin_sorteos.ex
+++ lib/admin_sorteos.ex
@@ -1,17 +1,62 @@
 defmodule AzarSa.AdminSorteos do
-  alias AzarSa.Storage #Utilizamos alias para acceder de forma más fácil al módulo.
-  @archivo "data/sorteos.json" #Se define el atributo del módulo para acceder a la ruta de sorteos, similar a una constante.
+  @moduledoc """
+  Módulo encargado de la administración operativa y financiera de los sorteos en el sistema AzarSa.
 
-  #Para recordar:
-  #Lambdas: fn argumentos -> cuerpo_de_la_funcion end
-  #Se agrega \\ para utilizar @archivo como un parámetro opcional en caso de no pasar la ruta. Especificamente para las pruebas.
+  Proporciona las funcionalidades principales para gestionar el ciclo de vida de los sorteos
+  y realizar auditorías de los datos almacenados en los archivos JSON.
+
+  ## Responsabilidades principales:
+  * **Gestión de Sorteos:** Listado, creación segura (evitando duplicados) y eliminación
+    de sorteos (aplicando reglas de negocio: no eliminar si ya hay premios configurados).
+  * **Análisis Financiero:** Cálculo de ingresos totales generados por la venta de billetes
+    de un sorteo específico.
+  * **Auditoría de Clientes:** Identificación rápida de los usuarios que han participado en un sorteo.
+  * **Balances y Cuadres:** Generación del balance neto (Ingresos totales - Costo de los premios)
+    para determinar la rentabilidad del sorteo.
+  """
+
+  # Utilizamos alias para acceder de forma más fácil al módulo.
+  alias AzarSa.Storage
+  # Se define el atributo del módulo para acceder a la ruta de sorteos, similar a una constante.
+  @archivo_sorteos "data/sorteos.json"
+  @archivo_usuarios "data/usuarios.json"
+
+  # Para recordar:
+  # Lambdas: fn argumentos -> cuerpo_de_la_funcion end
+  # Se agrega \\ para utilizar @archivo_sorteos o @archivo_usuarios como un parámetro opcional en caso de no pasar la ruta. Especificamente para las pruebas.
+  #
+  # --- ESTRUCTURAS DE ENUM UTILIZADAS ---
+  #
+  # 1. Enum.any?(enumerable, fn elemento -> condicion end)
+  #    -> Devuelve `true` si AL MENOS UN elemento cumple la condición. De lo contrario, `false`.
+  #
+  # 2. Enum.find(enumerable, fn elemento -> condicion end)
+  #    -> Devuelve el PRIMER elemento completo que cumpla la condición. Si no hay, devuelve `nil`.
+  #
+  # 3. Enum.empty?(enumerable)
+  #    -> Devuelve `true` si la lista está vacía ([]). Si tiene elementos, devuelve `false`.
+  #
+  # 4. Enum.reject(enumerable, fn elemento -> condicion end)
+  #    -> Lo opuesto a filter. Devuelve una lista ELIMINANDO los elementos que cumplan la condición.
+  #
+  # 5. Enum.flat_map(enumerable, fn elemento -> extraer_lista end)
+  #    -> Recorre el enumerable, extrae listas anidadas y las "aplana" en una sola lista continua.
+  #
+  # 6. Enum.filter(enumerable, fn elemento -> condicion end)
+  #    -> Actúa como un colador. Devuelve una lista SOLO con los elementos que cumplan la condición.
+  #
+  # 7. Enum.reduce(enumerable, valor_inicial, fn elemento, acumulador -> operacion end)
+  #    -> Recorre la lista arrastrando un "acumulador". Ideal para sumar totales o consolidar datos.
+  #
+  # 8. Enum.map(enumerable, fn elemento -> transformacion end)
+  #    -> Transforma cada elemento de la lista original y devuelve una nueva lista del mismo tamaño con los cambios.
 
   @doc """
   Lee el archivo JASON y devuelve la liusta de todos los sorteos.
   Si el archivo no existe o hay un error, devuelve una lista vacía.
   """
 
-  def listar_sorteos(ruta \\ @archivo) do
+  def listar_sorteos(ruta \\ @archivo_sorteos) do
     Storage.leer_json(ruta)
   end
 
@@ -19,14 +64,16 @@ defmodule AzarSa.AdminSorteos do
   Recibe un mapa con los datos del nuevo sorteo y lo guarda en el JSON.
   Valida que no exista ya un sorteo con el mismo ID.
   """
-  def crear_sorteo(nuevo_sorteo, ruta \\ @archivo) do
-    sorteos_actuales= listar_sorteos(ruta)
-    #Se verifica que el ID no exista previamente. Ingresa al mapa de sorteos_actuales y verifica si el ID concide con el nuevo.
-    if Enum.any?(sorteos_actuales, fn sorteo-> sorteo["id"] == nuevo_sorteo["id"] end) do
+  def crear_sorteo(nuevo_sorteo, ruta \\ @archivo_sorteos) do
+    sorteos_actuales = listar_sorteos(ruta)
+
+    # Se verifica que el ID no exista previamente. Ingresa al mapa de sorteos_actuales y verifica si el ID concide con el nuevo.
+    if Enum.any?(sorteos_actuales, fn sorteo -> sorteo["id"] == nuevo_sorteo["id"] end) do
       {:error, "Ya existe un sorteo con el ID: #{nuevo_sorteo["id"]}"}
     else
-      #De lo contrario se agrega el nuevo sorteo a la lista en la cabeza de la misma.
-      sorteos_actualizados= [nuevo_sorteo | sorteos_actuales]
+      # De lo contrario se agrega el nuevo sorteo a la lista en la cabeza de la misma.
+      sorteos_actualizados = [nuevo_sorteo | sorteos_actuales]
+
       case Storage.guardar_json(ruta, sorteos_actualizados) do
         :ok -> {:ok, "Sorteo creado exitosamente"}
         error -> error
@@ -34,15 +81,14 @@ defmodule AzarSa.AdminSorteos do
     end
   end
 
-
-  @doc"""
+  @doc """
   Elimina el sorteo por su ID.
   Aplica la la siguiente regla: No se puede eliminar si ya tiene premios asociados.
   """
 
-  def eliminar_sorteo(id_sorteo, ruta \\ @archivo) do
-    sorteos_actuales= listar_sorteos(ruta)
-    sorteo= Enum.find(sorteos_actuales, fn sorteo-> sorteo["id"] == id_sorteo end)
+  def eliminar_sorteo(id_sorteo, ruta \\ @archivo_sorteos) do
+    sorteos_actuales = listar_sorteos(ruta)
+    sorteo = Enum.find(sorteos_actuales, fn sorteo -> sorteo["id"] == id_sorteo end)
 
     case sorteo do
       nil ->
@@ -50,17 +96,181 @@ defmodule AzarSa.AdminSorteos do
 
       sorteo_encontrado ->
         # Se busca si tiene premios. Los premios vienen en una lista bajo la llave "premios"
-        premios= Map.get(sorteo_encontrado, "premios", [])
+        premios = Map.get(sorteo_encontrado, "premios", [])
+
         if Enum.empty?(premios) do
-          #Si no tiene premios asociados, se filtra la lista para quitar el sorteo.
-          sorteos_actualizados= Enum.reject(sorteos_actuales, fn sorteo-> sorteo["id"] == id_sorteo end)
+          # Si no tiene premios asociados, se filtra la lista para quitar el sorteo.
+          sorteos_actualizados =
+            Enum.reject(sorteos_actuales, fn sorteo -> sorteo["id"] == id_sorteo end)
+
           Storage.guardar_json(ruta, sorteos_actualizados)
           {:ok, "Sorteo eliminado exitosamente"}
-
-          else
-          #Se cumple la restricción, no se puede eliminar.
+        else
+          # Se cumple la restricción, no se puede eliminar.
           {:error, "No se puedse eliminar el sorteo porque ya tiene premios asociados"}
         end
+    end
+  end
+
+  @doc """
+  Calcula el total de ingresos recaudados para un sorteo en específico.
+  Busca en el archivo de usuarios todas las compras asociadas al ID del sorteo.
+  u= Un usuario individual.
+  acum= acumulador.
+  """
+  def consultar_ingresos(id_sorteo, ruta_usuarios \\ @archivo_usuarios) do
+    # Se carga la estructura de los usuarios
+    data = Storage.leer_json(ruta_usuarios)
+    usuarios = Map.get(data, "usuarios", [])
+
+    # Se extraen todas las compras de todos los usuarios
+    todas_las_compras = Enum.flat_map(usuarios, fn u -> Map.get(u, "compras", []) end)
+
+    # Se filtra el ID del sorteo y se suma el vlor pagado al total (acum)
+    todas_las_compras
+    |> Enum.filter(fn compra -> compra["id_sorteo"] == id_sorteo end)
+    |> Enum.reduce(0, fn compra, acum -> acum + compra["valor_pagado"] end)
+  end
+
+  @doc """
+  Devuelve una lista con el nombre de los clientes que han participado en un sorteo.
+  c= Una compra individual.
+  u= Un usuario individual.
+  """
+  def listar_clientes_por_sorteo(id_sorteo, ruta_usuarios \\ @archivo_usuarios) do
+    data = Storage.leer_json(ruta_usuarios)
+    usuarios = Map.get(data, "usuarios", [])
+
+    usuarios
+    |> Enum.filter(fn u ->
+      compras = Map.get(u, "compras", [])
+      Enum.any?(compras, fn c -> c["id_sorteo"] == id_sorteo end)
+    end)
+    # Se transforma cada mapa de usuario obtenido en un String unicamente con el nombre.
+    |> Enum.map(fn usuario -> usuario["nombre"] end)
+  end
+
+  @doc """
+  Calcula el balance total (ingresos - valor de premios) de un sorteo.
+  p= Un premio individual.
+  acum= acumulador.
+  s= Un sorteo individual.
+  """
+  def balance_sorteo(
+        id_sorteo,
+        ruta_sorteos \\ @archivo_sorteos,
+        ruta_usuarios \\ @archivo_usuarios
+      ) do
+    # Primero se obtienen los ingresos.
+    ingresos = consultar_ingresos(id_sorteo, ruta_usuarios)
+
+    # Se obtiene el valor total de los premios por sorteo
+    sorteos = listar_sorteos(ruta_sorteos)
+    sorteo = Enum.find(sorteos, fn s -> s["id"] == id_sorteo end)
+
+    case sorteo do
+      nil ->
+        {:error, "Sorteo no encontrado"}
+
+      s ->
+        premios = Map.get(s, "premios", [])
+        total_premios = Enum.reduce(premios, 0, fn p, acum -> acum + p["valor"] end)
+
+        %{
+          "id_sorteo" => id_sorteo,
+          "ingresos" => ingresos,
+          "costo_premios" => total_premios,
+          "balance_neto" => ingresos - total_premios
+        }
+    end
+  end
+
+  # Admin premios
+  @doc """
+  Lista todos los premios configurados para un sorteo específico.
+  """
+  def listar_premios(id_sorteo, ruta_sorteos \\ @archivo_sorteos) do
+    # Primero listamos todos los sorteos
+    sorteos = listar_sorteos(ruta_sorteos)
+    # Buscamos dentro de la lista de sorteos el que coincida con el id de sorteo solicitado.
+    sorteo = Enum.find(sorteos, fn s -> s["id"] == id_sorteo end)
+
+    case sorteo do
+      nil -> {:error, "Sorteo no encontrado"}
+      # Se extrae la lista de premios del sorteo encotnrado.
+      s -> Map.get(s, "premios", [])
+    end
+  end
+
+  @doc """
+  Agrega un nuevo premio a la lista de premios de un sorteo.
+  """
+
+  def agregar_premio(id_sorteo, nuevo_premio, ruta_sorteos \\ @archivo_sorteos) do
+    # Listamos todos los sorteos.
+    sorteos = listar_sorteos(ruta_sorteos)
+
+    # En la variable sorteos_actualizados se guardarán los sorteos anteriores + el nuevo sorteo siempre que el id coincida.
+    sorteos_actualizados =
+      Enum.map(sorteos, fn s ->
+        if s["id"] == id_sorteo do
+          # Se extraen los premios actuales para poder agregar el nuevo premio en el siguiente paso.
+          premios_actuales = Map.get(s, "premios", [])
+          # Se agrega el nuevo premio a la lista
+          Map.put(s, "premios", [nuevo_premio | premios_actuales])
+        else
+          # De lo contrario los demás sorteos pasan iguales.
+          s
+        end
+      end)
+
+    Storage.guardar_json(ruta_sorteos, sorteos_actualizados)
+  end
+
+  @doc """
+  Elimina un premio por su nombre, siempre y cuando el sorteo no tenga ventas.
+  """
+  def eliminar_premio(
+        id_sorteo,
+        nombre_premio,
+        ruta_sorteos \\ @archivo_sorteos,
+        ruta_usuarios \\ @archivo_usuarios
+      ) do
+    # Se obtiene la lista de usuarios
+    data_usuarios = Storage.leer_json(ruta_usuarios)
+    usuarios = Map.get(data_usuarios, "usuarios", [])
+
+    # Se verifica si existe algún cliente para el sorteo. Extrae las compras del usuario y luego revisa si en esas compras alguna coincide con el id del sorteo.
+    tiene_clientes =
+      Enum.any?(usuarios, fn u ->
+        compras = Map.get(u, "compras", [])
+        Enum.any?(compras, fn c -> c["id_sorteo"] == id_sorteo end)
+      end)
+
+    #Verifica que si tiene clientes, entonces no se puede eliminar el premio. De lo contrario, se procede a eliminarlo.
+    if tiene_clientes do
+      {:error, "No se puede eliminar el premio porque hay clientes participando en el sorteo"}
+    else
+      # Si no hay clientes, entonces se procede a eliminar el premio. Se listan primero los sorteos.
+      sorteos = listar_sorteos(ruta_sorteos)
+
+      # sorteos_actualizados guardará los sorteos nuevos cuando se elimine el premio requerido.
+      sorteos_actualizados =
+        Enum.map(sorteos, fn s ->
+          if s["id"] == id_sorteo do
+            #Usando rejects, se filtra la lista de premios para eliminar el premio que coincida con el nombre del premio a eliminar.
+            premios_filtrados =
+              Enum.reject(s["premios"], fn p -> p["nombre"] == nombre_premio end)
+            # Se actualiza el sorteo con la nueva lista de premios filtrada.
+            Map.put(s, "premios", premios_filtrados)
+          else
+            # De lo contrario, los demás sorteos pasan igual.
+            s
+          end
+        end)
+
+      Storage.guardar_json(ruta_sorteos, sorteos_actualizados)
+      {:ok, "Premio eliminado exitosamente"}
     end
   end
 end
--- test/admin_sorteos_test.exs
+++ test/admin_sorteos_test.exs
@@ -4,6 +4,7 @@ defmodule AzarSa.AdminSorteosTest do
   alias AzarSa.AdminSorteos
   alias AzarSa.Storage
 
+  @prueba_usuarios "data/dummy_usuarios.json"
   @prueba_sorteos "data/dummy_sorteos.json"
 
   describe "crear_sorteo/2" do
@@ -84,6 +85,162 @@ defmodule AzarSa.AdminSorteosTest do
 
       # Se elimina el archivo al finalizar.
       File.rm(@prueba_sorteos)
+    end
+  end
+
+  describe "Funciones de análisis financiero y clientes/4" do
+    # Se crea el bloque setup que se ejecutará antes de cada pruieba de este bloque, esto con el fin de crear el archivo dummy necesario.
+    setup do
+      # Se prepara el archivo dummy de los sorteos.
+      sorteos = [
+        %{
+          "id" => "S-001",
+          "premios" => [
+            # Total de premios: $70.000
+            %{"valor" => 50_000},
+            %{"valor" => 20_000}
+          ]
+        }
+      ]
+
+      # Se guarda en un nuevo archivo dummy el sorteo previamente creado.
+      Storage.guardar_json(@prueba_sorteos, sorteos)
+
+      # Se prepara un archivo de usuairos Dummy
+      usuarios = %{
+        "usuarios" => [
+          %{
+            "nombre" => "Pedro",
+            "compras" => [
+              %{"id_sorteo" => "S-001", "valor_pagado" => 20_000},
+              %{"id_sorteo" => "S-001", "valor_pagado" => 20_000}
+            ]
+          },
+          %{
+            "nombre" => "Pablo",
+            "compras" => [
+              %{"id_sorteo" => "S-001", "valor_pagado" => 60_000},
+              %{"id_sorteo" => "S-002", "valor_pagado" => 15_000}
+            ]
+          },
+          %{
+            "nombre" => "Carmen",
+            "compras" => [
+              %{"id_sorteo" => "S-002", "valor_pagado" => 30_000}
+            ]
+          }
+        ]
+      }
+
+      # Se guarda el archivo de usuarios en la ruta dummy.
+      Storage.guardar_json(@prueba_usuarios, usuarios)
+
+      # Aseguramos que después de cada test los archivos se borren.
+      on_exit(fn ->
+        File.rm(@prueba_sorteos)
+        File.rm(@prueba_usuarios)
+      end)
+
+      :ok
+    end
+
+    test "consultar_ingresos suma correctamente el valor de los billetes" do
+      # La suma total para el sorteo "S-001" es 100.000
+      assert AdminSorteos.consultar_ingresos("S-001", @prueba_usuarios) == 100_000
+      # La suma total para el sorteo "S-002" es 45.000
+      assert AdminSorteos.consultar_ingresos("S-002", @prueba_usuarios) == 45_000
+      # Prueba con un sorteo que no tiene compras
+      assert AdminSorteos.consultar_ingresos("S-000", @prueba_usuarios)
+    end
+
+    test "listar_clientes_por_sorteo devuelve solo los nombres de los compradores correctos" do
+      # Se obtiene la lista de clientes, en este caso para el sorteo "S-001".
+      clientes_sorteo1 = AdminSorteos.listar_clientes_por_sorteo("S-001", @prueba_usuarios)
+
+      assert length(clientes_sorteo1) == 2
+      assert Enum.member?(clientes_sorteo1, "Pedro")
+      assert Enum.member?(clientes_sorteo1, "Pablo")
+
+      # Carmen no hace parte del sorteo "S-001", por lo que se usa 'refute' esperando obtener false.
+      refute Enum.member?(clientes_sorteo1, "Carmen")
+    end
+
+    test "balance_sorteo calcula la rentabilidad cruzando datos de ambos archivos" do
+      # El sorteo "S-001" recaudó 100.000 y sus premios cuestan 70.000. Balance = +30.000. Se obtiene el balance.
+      resultado = AdminSorteos.balance_sorteo("S-001", @prueba_sorteos, @prueba_usuarios)
+
+      # Se compara cada uno de los datos obtenidos.
+      assert resultado["id_sorteo"] == "S-001"
+      assert resultado["ingresos"] == 100_000
+      assert resultado["costo_premios"] == 70_000
+      assert resultado["balance_neto"] == 30_000
+    end
+
+    test "balance_sorteo devuelve error si el sorteo no existe" do
+      assert {:error, "Sorteo no encontrado"} ==
+               AdminSorteos.balance_sorteo("S-XYZ", @prueba_sorteos, @prueba_usuarios)
+    end
+  end
+
+
+  describe "Gestión de premios Admin/3" do
+    setup do
+      # Se crea el sorteo S-001 con un premio inicial
+      sorteos = [
+        %{
+          "id" => "S-001",
+          "nombre" => "Sorteo Test",
+          "premios" => [%{"nombre" => "Premio Base", "valor" => 10_000}]
+        }
+      ]
+      Storage.guardar_json(@prueba_sorteos, sorteos)
+
+      # 2. Escenario de Usuarios: Sin compras inicialmente
+      Storage.guardar_json(@prueba_usuarios, %{"usuarios" => []})
+
+      on_exit(fn ->
+        File.rm(@prueba_sorteos)
+        File.rm(@prueba_usuarios)
+      end)
+
+      :ok
+    end
+
+    test "agregar_premio inserta un nuevo premio en el sorteo correcto" do
+      nuevo_premio= %{"nombre" => "Premio Mayor", "valor" => 500_000}
+      AdminSorteos.agregar_premio("S-001", nuevo_premio, @prueba_sorteos)
+
+      premios= AdminSorteos.listar_premios("S-001", @prueba_sorteos)
+      # Se verifica que el nuevo premio se haya agregado correctamente al sorteo S-001.
+      assert Enum.any?(premios, fn p-> p["nombre"] == "Premio Mayor" and p["valor"] == 500_000 end)
+    end
+
+
+    test "eliminar_premio eliminar el premio si no hay clientes asociados al sorteo" do
+      #Se intenta eliminar el premio base de un sorteo sin ventas.
+      assert {:ok, "Premio eliminado exitosamente"} == AdminSorteos.eliminar_premio("S-001", "Premio Base", @prueba_sorteos, @prueba_usuarios)
+
+      #Lista los premios
+      premios= AdminSorteos.listar_premios("S-001", @prueba_sorteos)
+      #A través del refute, esperando un false como respuesta, se verifica que el premio ya no esté en la lista de premios del sorteo S-001.
+      refute Enum.any?(premios, fn p -> p["nombre"] == "Premio Base" end)
+    end
+
+    test "eliminar_premio no eliminar el premio si hay clientes asociados al sorteo" do
+      #Primero se crea y guarda un usuario con compra para el sorteo S-001.
+      usuarios_con_venta = %{
+        "usuarios" => [
+          %{"nombre" => "Juan", "compras" => [%{"id_sorteo" => "S-001", "valor_pagado" => 5_000}]}
+        ]
+      }
+      Storage.guardar_json(@prueba_usuarios, usuarios_con_venta)
+
+      #Se intenta eliminar el premio.
+      assert{{:error, "No se puede eliminar el premio porque hay clientes participando en el sorteo"} == AdminSorteos.eliminar_premio("S-001", "Premio Base", @prueba_sorteos, @prueba_usuarios)}
+
+      #Se verifica que el premio siga en la lista de premios del sorteo S-001
+      premios= AdminSorteos.listar_premios("S-001", @prueba_sorteos)
+      assert Enum.any?(premios, fn p -> p["nombre"] == "Premio Base" end)
     end
   end
 end
