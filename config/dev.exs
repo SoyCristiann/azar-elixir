78faba62479e8023ff62b3fe1995f6ace473d5f5
Cristian Navarro <navarro_cristian@outlook.com>
2026-05-20 23:39:10 -0500

    Ajuste de config para github workflows

--- config/dev.exs
+++ config/dev.exs
@@ -47,13 +47,10 @@ config :azar_elixir, AzarElixirWeb.Endpoint,
   live_reload: [
     web_console_logger: true,
     patterns: [
-      # Static assets, except user uploads
-      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
-      # Gettext translations
-      ~r"priv/gettext/.*\.po$"E,
-      # Router, Controllers, LiveViews and LiveComponents
-      ~r"lib/azar_elixir_web/router\.ex$"E,
-      ~r"lib/azar_elixir_web/(controllers|live|components)/.*\.(ex|heex)$"E
+      ~r/priv\/static\/(?!uploads\/).*\.(js|css|png|jpeg|jpg|gif|svg)$/,
+      ~r/priv\/gettext\/.*\.po$/,
+      ~r/lib\/azar_elixir_web\/router\.ex$/,
+      ~r/lib\/azar_elixir_web\/(controllers|live|components)\/.*\.(ex|heex)$/
     ]
   ]
 
